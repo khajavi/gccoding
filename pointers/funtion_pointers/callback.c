2593fbc955f4b267c8fd4aea40108ed8edf46bce
Milad Khajavi <khajavi@gmail.com>
2013-02-20 16:59:09 +0330

    callback.c is modified.

--- pointers/funtion_pointers/callback.c
+++ pointers/funtion_pointers/callback.c
@@ -1,5 +1,5 @@
 /*
- * Adopted from http://stackoverflow.com/a/2738486/225052 licensed uder cc-by-sa 3.0
+ * Adopted from http://stackoverflow.com/a/2738486/225052 licensed under cc-by-sa 3.0
  */
 #include <stdio.h>
 
@@ -18,6 +18,7 @@ void reg(callback_t _callback) {
 int main() {
 	callback = callback_proc;
 	reg(callback);
+	reg(callback_proc);
 
 	return 0;
 }
