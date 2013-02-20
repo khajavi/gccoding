320b45bae7feb98763e527736100c67e98b594d2
Milad Khajavi <khajavi@gmail.com>
2013-02-20 16:17:01 +0330

    callback.c is added

---
+++ pointers/funtion_pointers/callback.c
@@ -0,0 +1,23 @@
+/*
+ * Adopted from http://stackoverflow.com/a/2738486/225052 licensed uder cc-by-sa 3.0
+ */
+#include <stdio.h>
+
+typedef void (*callback_t)(void);
+callback_t callback;
+
+void callback_proc(void) {
+	printf("Inside callback funtion\n");
+}
+
+void reg(callback_t _callback) {
+	printf("Inside registration\n");
+	_callback();
+}
+
+int main() {
+	callback = callback_proc;
+	reg(callback);
+
+	return 0;
+}
