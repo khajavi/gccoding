b51f1331119bc0e2c5b38e4f6ab5af1813fc9fb9
Milad Khajavi <khajavi@gmail.com>
2013-02-21 20:46:59 +0330

    functor.cpp is added.

---
+++ C++/functor.cpp
@@ -0,0 +1,20 @@
+/*
+ * Adopted from http://stackoverflow.com/a/356992/225052 lisenced under cc-by-sa 3.0
+ */
+#include <iostream>
+
+class MyFunctor {
+	public:
+		int operator()(int x) {
+			return x*2;
+		}
+};
+
+int main() {
+
+	MyFunctor doubler;
+	int x = doubler(5);
+	std::cout << x << std::endl;
+
+	return 0;
+}
